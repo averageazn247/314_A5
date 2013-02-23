5318314e3283e976547044af51459cc06584ca3f
Christopher Lenart <clenart1@icloud.com>
2013-02-23 20:35:32 +0100

    parentheses work, and added transform function to take care of associativity

--- Parser.hs
+++ Parser.hs
@@ -8,14 +8,23 @@ import Data.Maybe
 main = do
   [f] <- getArgs
   s   <- readFile f
-  writeFile "output.txt" (printAST (fst (parse_expression (lexer s))))
+  writeFile "output.txt" (printAST (transformAST(fst (parse_expression (lexer s)))) "")
+
+transformAST :: AST -> AST
+transformAST (NODE SUBTRACTION left (NODE ADDITION inner right)) = NODE ADDITION (NODE SUBTRACTION (transformAST left) (transformAST inner)) (transformAST right)
+transformAST (NODE DIVISION left (NODE MULTIPLICATION inner right)) = NODE MULTIPLICATION (NODE DIVISION (transformAST left) (transformAST inner)) (transformAST right)
+transformAST (NODE ADDITION y z) = NODE ADDITION (transformAST y) (transformAST z)
+transformAST (NODE MULTIPLICATION y z) = NODE MULTIPLICATION (transformAST y) (transformAST z)
+transformAST (NODE SUBTRACTION y z) = NODE SUBTRACTION (transformAST y) (transformAST z)
+transformAST (NODE DIVISION y z) = NODE DIVISION (transformAST y) (transformAST z)
+transformAST (FLOAT x) = FLOAT x
+transformAST (VAR x) = VAR x
+transformAST EMPTY = EMPTY
 
 parse_expression :: [Token] -> (AST, [Token])
 parse_expression input 
   | input == [] = (EMPTY, [])
-  | head input == LPAREN = parse_expression (tail input)
   |      result_leftovers == [] = parse_term input
-  | head result_leftovers == RPAREN = (fst (parse_primary [head input]), tail result_leftovers)
   | head result_leftovers == ADDITION = (NODE ADDITION result_AST (fst (parse_expression (tail result_leftovers))), snd (parse_expression (tail result_leftovers)))
   | head result_leftovers == SUBTRACTION = (NODE SUBTRACTION result_AST (fst (parse_expression (tail result_leftovers))), snd (parse_expression (tail result_leftovers)))
   | otherwise = parse_term input
@@ -26,9 +35,7 @@ parse_expression input
 parse_term :: [Token] -> (AST, [Token])
 parse_term input
   | input == [] = (EMPTY, [])
-  | head input == LPAREN = parse_expression (tail input)
   |      result_leftovers == [] = parse_primary input
-  | head result_leftovers == RPAREN = (fst (parse_primary [head input]), tail result_leftovers)
   | head result_leftovers == MULTIPLICATION = (NODE MULTIPLICATION result_AST (fst (parse_term (tail result_leftovers))), snd (parse_term (tail result_leftovers)))
   | head result_leftovers == DIVISION = (NODE DIVISION result_AST (fst (parse_term (tail result_leftovers))), snd (parse_term (tail result_leftovers)))
   | otherwise = parse_primary input
@@ -36,8 +43,15 @@ parse_term input
         result_AST = fst primary_result
         result_leftovers = snd primary_result
 
+parse_parentheses :: [Token] -> Int -> Int -> Int
+parse_parentheses input level count
+  | level == 0 = count
+  | head input == LPAREN = parse_parentheses (tail input) (level + 1) (count + 1)
+  | head input == RPAREN = parse_parentheses (tail input) (level - 1) (count + 1)
+  | otherwise = parse_parentheses (tail input) level (count + 1)
+
 parse_primary :: [Token] -> (AST, [Token])
-parse_primary input
-  | input == [] = (EMPTY, [])
+parse_primary [] = (EMPTY, [])
+parse_primary (LPAREN:xs) = (fst (parse_expression (take ((parse_parentheses xs 1 0)-1) xs)), drop ((parse_parentheses xs 1 0)) xs)
 parse_primary (FP x:xs) = (FLOAT x, xs)
 parse_primary (VARIABLE x:xs) = (VAR x, xs)
