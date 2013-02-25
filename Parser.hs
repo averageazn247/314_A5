b35fccbe76cc17aa630ccdf70f164fbf7b88a8ef
Christopher Lenart <clenart1@icloud.com>
2013-02-25 05:08:39 +0100

    added some error checking

--- Parser.hs
+++ Parser.hs
@@ -40,8 +40,10 @@ transformAST EMPTY = EMPTY
 
 parse_expression :: [Token] -> (AST, [Token])
 parse_expression input 
-  | input == [] = (EMPTY, [])
+  | input == [] = error "Expected: expression, Actual: end of input"
   |      result_leftovers == [] = parse_term input
+  | head result_leftovers == LPAREN = error "Expected: expression operator, Actual: left parentheses"
+  | head result_leftovers == RPAREN = error "Expected: expression operator, Actual: right parentheses"
   | head result_leftovers == ADDITION = (NODE ADDITION result_AST (fst (parse_expression (tail result_leftovers))), snd (parse_expression (tail result_leftovers)))
   | head result_leftovers == SUBTRACTION = (NODE SUBTRACTION result_AST (fst (parse_expression (tail result_leftovers))), snd (parse_expression (tail result_leftovers)))
   | otherwise = parse_term input
@@ -51,8 +53,10 @@ parse_expression input
 
 parse_term :: [Token] -> (AST, [Token])
 parse_term input
-  | input == [] = (EMPTY, [])
+  | input == [] = error "Expected: term, Actual: end of input"
   |      result_leftovers == [] = parse_primary input
+  | head result_leftovers == LPAREN = error "Expected: term operator, Actual: left parentheses"
+  | head result_leftovers == RPAREN = error "Expected: term operator, Actual: right parentheses"
   | head result_leftovers == MULTIPLICATION = (NODE MULTIPLICATION result_AST (fst (parse_term (tail result_leftovers))), snd (parse_term (tail result_leftovers)))
   | head result_leftovers == DIVISION = (NODE DIVISION result_AST (fst (parse_term (tail result_leftovers))), snd (parse_term (tail result_leftovers)))
   | otherwise = parse_primary input
@@ -60,16 +64,18 @@ parse_term input
         result_AST = fst primary_result
         result_leftovers = snd primary_result
 
+parse_primary :: [Token] -> (AST, [Token])
+parse_primary [] = error "Expected: primary, Actual: end of input"
+parse_primary (LPAREN:xs) = (fst (parse_expression (take ((parse_parentheses xs 1 0)-1) xs)), drop ((parse_parentheses xs 1 0)) xs)
+parse_primary (FP x:xs) = (FLOAT x, xs)
+parse_primary (VARIABLE x:xs) = (VAR x, xs)
+parse_primary (x:xs) = error ("Expected: primary, Actual: " ++ token_to_string x)
+
 -- returns an int specifying the number of elements in the input that are part of the top parentheses level
 parse_parentheses :: [Token] -> Int -> Int -> Int
 parse_parentheses input level count
   | level == 0 = count
+  | input == [] = error "Expected: right parentheses, Actual: end of input"
   | head input == LPAREN = parse_parentheses (tail input) (level + 1) (count + 1)
   | head input == RPAREN = parse_parentheses (tail input) (level - 1) (count + 1)
   | otherwise = parse_parentheses (tail input) level (count + 1)
-
-parse_primary :: [Token] -> (AST, [Token])
-parse_primary [] = (EMPTY, [])
-parse_primary (LPAREN:xs) = (fst (parse_expression (take ((parse_parentheses xs 1 0)-1) xs)), drop ((parse_parentheses xs 1 0)) xs)
-parse_primary (FP x:xs) = (FLOAT x, xs)
-parse_primary (VARIABLE x:xs) = (VAR x, xs)
